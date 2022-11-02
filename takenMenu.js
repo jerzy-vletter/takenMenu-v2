0ebdc3f2bb6fb3b019a00aa49bd20c6b187878b5
jerzy-vletter <vletter123@gmail.com>
2022-11-02 23:19:23 +0100

    draft2 wip

--- takenMenu.js
+++ takenMenu.js
@@ -1,5 +1,17 @@
 window.onload =  function(){
-    test1.addEventListener('click', ()=>{
-        alert();
+    header.addEventListener('click', ()=>{
+        document.getElementById("initPageContainer").style.display = "flex";
+        document.getElementById("taakstraffenContainer").style.display = "none";
+        document.getElementById("gevangenisstraffenContainer").style.display = "none";
+    });
+
+    taakstraffen.addEventListener('click', ()=>{
+        document.getElementById("taakstraffenContainer").style.display = "flex";
+        document.getElementById("initPageContainer").style.display = "none";
+    });
+
+    gevangenisstraffen.addEventListener('click', ()=>{
+        document.getElementById("gevangenisstraffenContainer").style.display = "flex";
+        document.getElementById("initPageContainer").style.display = "none";
     });
 };
